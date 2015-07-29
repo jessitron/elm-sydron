751dc21015fc0234956df353498783a2adf82a60
Jessica Kerr <jessitron@outpace.com>
2015-07-29 12:20:24 +0000

    start working on images view

---
+++ SeeThePeople.elm
@@ -0,0 +1,20 @@
+module SeeThePeople where
+
+import GithubEvent exposing (EventActor)
+import Html exposing (Html)
+import Html.Attributes as Attr
+
+type alias Model = 
+    { 
+      allActors: List EventActor
+    }
+
+---- VIEW
+
+draw: EventActor -> Html
+draw actor = Html.img [Attr.src actor.avatar_url] []
+
+view: Model -> Html
+view model =
+  Html.div [] (List.map draw model.allActors)
+
