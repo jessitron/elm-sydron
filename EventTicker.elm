df040e59971f6cfe9b78f45043adbfc21297b2c5
Jessica Kerr <jessitron@outpace.com>
2015-07-30 03:05:31 +0000

    put a box around ticker
    also discovered that if I set 'overflow' to 'scroll' then it doesn't suck

--- EventTicker.elm
+++ EventTicker.elm
@@ -3,12 +3,13 @@ module EventTicker(Model, init, view, update) where
 import GithubEvent exposing (Event)
 import GithubEventSignal exposing (SingleEvent(..))
 import Html exposing (Html)
+import Html.Attributes exposing (style)
 
 -- Model
 
 type alias Model = 
   {
-    upToThreeRecentEvents : List Event
+    recentEvents : List Event
   }
 init: Model 
 init = Model []
@@ -16,13 +17,31 @@ init = Model []
 -- View
 eventListItem : Event -> Html
 eventListItem event =
-    Html.li [] [Html.text (event.eventType ++ " by " ++ event.actor.login)]
+    Html.li [itemStyle]
+    [Html.text (event.eventType ++ " by " ++ event.actor.login)]
 
 view : Model -> Html
 view m =
     Html.div
-        [ ]
-        [ Html.h2 [] (List.map eventListItem m.upToThreeRecentEvents) ]
+        [ divStyle ]
+        [ Html.ul [] (List.map eventListItem m.recentEvents) ]
+
+divStyle = 
+  style 
+    [  
+      ("height", "100px"),
+      ("box-sizing", "border-box"),
+      ("border", "1px solid"),
+      ("color", "#666666"),
+      ("overflow", "scroll")
+    ]
+itemStyle = 
+  style 
+    [ 
+      ("color", "#515151"),
+      ("font-family", "Helvetica"),
+      ("font-size", "14pt")
+    ]
 
 -- Update
 type alias Action = GithubEventSignal.SingleEvent
@@ -31,4 +50,4 @@ update: Action -> Model -> Model
 update action model =
     case action of
         NothingYet -> model
-        SoThisHappened event -> Model (List.take 3 (event :: model.upToThreeRecentEvents))
+        SoThisHappened event -> Model (List.take 10 (event :: model.recentEvents))
