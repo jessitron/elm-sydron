609871e25d7edf06ed37a58751e5e21e8ef2ede2
Jessica Kerr <jessitron@outpace.com>
2015-08-16 11:27:32 +0000

    Remove the old event-fetching

--- src/Sydron.elm
+++ src/Sydron.elm
@@ -2,7 +2,6 @@ module Sydron where
 
 import SydronAction exposing (SydronAction(..))
 import GithubEventLayer
-import GithubEventSignal exposing (SingleEvent(..))
 import GithubRepository exposing (GithubRepository) 
 import Task
 import Http
@@ -20,7 +19,7 @@ app = StartApp.start (StartApp.Config
                        (GithubEventLayer.init repositoryOfInterest)
                        GithubEventLayer.update
                        GithubEventLayer.view
-                       [both])
+                       [animationFrames])
 main = app.html
 
 port tasks : Signal (Task.Task Never ())
@@ -36,15 +35,10 @@ repositoryOfInterest =
     (ParseUrlParams.parse initialLocation) 
     "satellite-of-love" "Hungover"
 
-port githubEventsPort : Signal (Task.Task Http.Error ())
-port githubEventsPort = GithubEventSignal.fetchOnce repositoryOfInterest
-
 timePasses : Signal Time
 timePasses =  (Signal.map Time.inMilliseconds (Time.fps 30))
 
-both : Signal GithubEventLayer.Action
-both = Signal.merge 
-         (Signal.map TimeKeepsTickingAway timePasses)
-         (Signal.map SingleEvent GithubEventSignal.eventsOneByOne)
-       |> Signal.map GithubEventLayer.wrapAction 
+animationFrames : Signal GithubEventLayer.Action
+animationFrames = Signal.map TimeKeepsTickingAway timePasses
+                  |> Signal.map GithubEventLayer.wrapAction 
 
