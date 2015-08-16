21b10ce98c0c1ca3f5a9a44ec4f6269509555fa4
Jessica Kerr <jessitron@outpace.com>
2015-08-16 19:45:59 +0000

    add more details to the header

--- src/Header.elm
+++ src/Header.elm
@@ -23,7 +23,11 @@ view model =
     Html.a [Attr.href (repositoryLink model) ] [ Html.text (repositoryDescription model)],
     Html.text ". This is me playing with Elm; source code ",
     Html.a [Attr.href "http://github.com/jessitron/elm-sydron"] [ Html.text "here" ],
-    Html.text "."
+    Html.text ".",
+    Html.br [] [],
+    Html.text "This retrieves one page of past events (within the past 90 days; github doesn't keep them forever)",
+    Html.text " and displays them one at a time. Then it polls github for new events for the repository, displaying them as they come in.",
+    Html.text " Events are displayed at most one per three seconds."
   ]
 
 repositoryLink repo = "http://github.com/" ++ repo.owner ++ "/" ++ repo.repo
