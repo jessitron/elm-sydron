fa75413063f8e81cbdfff611c65ac92d46f50e4f
Jessica Kerr <jessitron@outpace.com>
2015-08-23 17:04:21 +0000

    Woohoo the ticker highlights too

--- src/EventTicker.elm
+++ src/EventTicker.elm
@@ -21,16 +21,31 @@ init =
 -- View
 
 -- TODO: make watch events show as (String.fromChar '\x2b50')
-eventListItem : Event -> Html
-eventListItem event =
-    Html.div [itemStyle]
+
+type alias StylePortion = List (String, String)
+
+eventListItem : (Event -> StylePortion) -> Event -> Html
+eventListItem howToHighlight event  =
+    Html.div [itemStyle (howToHighlight event)]
     [Html.text (event.eventType ++ " by " ++ event.actor.login ++ " at " ++ event.created_at)]
 
 view : Model -> Html
 view m =
+  let
+    howToHighlight event = 
+      case m.highlightPerson of
+        Nothing -> []
+        Just ea ->
+          if ea == event.actor then
+            highlightStyle
+          else
+           []
+  in
     Html.div
         [ divStyle ]
-        (List.map eventListItem m.recentEvents)
+        (List.map (eventListItem howToHighlight) m.recentEvents)
+
+highlightStyle = [("background-color", "gold")]
 
 divStyle = 
   style 
@@ -42,14 +57,15 @@ divStyle =
       ("overflow", "scroll"),
       ("padding", "10px")
     ]
-itemStyle = 
+itemStyle: StylePortion -> Html.Attribute
+itemStyle highlight = 
   style 
-    [ 
+    ([ 
       ("color", "#515151"),
       ("font-family", "Helvetica"),
       ("font-size", "21px"),
       ("height", "24px")
-    ]
+    ] ++ highlight)
 
 -- Update
 type alias Action = SydronAction
