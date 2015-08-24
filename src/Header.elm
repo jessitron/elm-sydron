ce855ebaa97f25e75d3b420cc2e2cae673742187
Richard Feldman <richard.t.feldman@gmail.com>
2015-08-23 21:08:23 -0700

    Swap intro paragraphs.

--- src/Header.elm
+++ src/Header.elm
@@ -20,14 +20,14 @@ view model =
   [
     h1 [] [text "Sydron: A Parade of GitHub Events"],
     p [] [
+      text "This retrieves one page of past events (within the past 90 days; GitHub doesn’t keep them forever) and displays them one at a time. Then it polls for new events for the repository, displaying them as they come in. Events are displayed at most one per three seconds."
+    ],
+    p [] [
       text "This is me playing with ",
       a [href "http://elm-lang.org"] [text "Elm"],
       text "; source code ",
       a [href "http://github.com/jessitron/elm-sydron"] [text "here"],
       text "."
-    ],
-    p [] [
-      text "This retrieves one page of past events (within the past 90 days; GitHub doesn’t keep them forever) and displays them one at a time. Then it polls for new events for the repository, displaying them as they come in. Events are displayed at most one per three seconds."
     ]
   ]
 
