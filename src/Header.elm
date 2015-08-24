5ef779c318db4b86f7e7a31668ce94a0ffb04400
Richard Feldman <richard.t.feldman@gmail.com>
2015-08-23 20:21:35 -0700

    Revise header copy.

--- src/Header.elm
+++ src/Header.elm
@@ -17,13 +17,17 @@ view model =
         "margin-left" => "20px"
       ]
     ]
-  [ 
-    h1 [] 
-      [text "Sydron"],
-    h2 [] 
-    [
-      text "A parade of Github Events for ",
-      a [href (repositoryLink model) ] [ text (repositoryLink model)]
+  [
+    h1 [] [text "Sydron: A Parade of GitHub Events"],
+    p [] [
+      text "This is me playing with ",
+      a [href "http://elm-lang.org"] [text "Elm"],
+      text "; source code ",
+      a [href "http://github.com/jessitron/elm-sydron"] [text "here"],
+      text "."
+    ],
+    p [] [
+      text "This retrieves one page of past events (within the past 90 days; GitHub doesn’t keep them forever) and displays them one at a time. Then it polls for new events for the repository, displaying them as they come in. Events are displayed at most one per three seconds."
     ]
   ]
 
