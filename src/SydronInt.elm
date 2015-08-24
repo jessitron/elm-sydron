412b25345c2a87d5421c2d9b4e318787a278af44
Richard Feldman <richard.t.feldman@gmail.com>
2015-08-23 17:55:54 -0700

    spans are like divs but with display:inline by default

--- src/SydronInt.elm
+++ src/SydronInt.elm
@@ -36,18 +36,13 @@ formclass = "pure-form" -- this is dependent on index.html including purecss
 
 view : Signal.Address SydronAction -> Model -> Html
 view addr m =
-  div
-    [ ]
-    [ 
-      Header.view m.repo,
-      RepoInput.view formclass,
-      Html.div [inline] [EventTicker.view addr m.ticker],
-      Html.div [inline] [SeeThePeople.view addr m.people]
-    ]
-
-
-inline: Attribute
-inline = style ["display" => "inline"]
+    div
+        [ ]
+        [ Header.view m.repo,
+          RepoInput.view formclass,
+          span [] [EventTicker.view addr m.ticker],
+          span [] [SeeThePeople.view addr m.people]
+        ]
 
 
 -- UPDATE
